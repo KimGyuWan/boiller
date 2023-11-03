e6127880ee5644fc7ed4e71fa91b52056bf119b1
KimGyuWan <y5g5776@naver.com>
2023-11-03 17:46:19 +0900

    A형 Swipper완료

--- js/web.js
+++ js/web.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
-    //화면이 준비되면 실행해라.
+
+  setInterval(function () {
+    $(".swipper-wrapper").animate({ "marginLeft": "-1200px" }, 400, function () {
+      $("div:first-child", this).appendTo($(this));
+      $(this).css("marginLeft", "0");
+    });
+  }, 3000);
+
 
 })
